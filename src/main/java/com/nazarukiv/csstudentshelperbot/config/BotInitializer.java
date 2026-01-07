f31cedc4798ac63bea34edf445d65c109d23cbfa
nazarukiv <nazaruk7649@ukr.net>
2026-01-07 20:35:04 +0000

    bot: add bot initializer and config

---
+++ src/main/java/com/nazarukiv/csstudentshelperbot/config/BotInitializer.java
@@ -0,0 +1,32 @@
+package com.nazarukiv.csstudentshelperbot.config;
+
+import com.nazarukiv.csstudentshelperbot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+@Slf4j
+public class BotInitializer {
+
+    private final TelegramBot bot;
+
+    public BotInitializer(TelegramBot bot) {
+        this.bot = bot;
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void init() {
+        try {
+            TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+            api.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: {}", e.getMessage(), e);
+        }
+    }
+}
---
+++ src/main/java/com/nazarukiv/csstudentshelperbot/service/TelegramBot.java
@@ -0,0 +1,52 @@
+package com.nazarukiv.csstudentshelperbot.service;
+
+import com.nazarukiv.csstudentshelperbot.config.BotConfig;
+import com.nazarukiv.csstudentshelperbot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            long chatId = update.getMessage().getChatId();
+            String messageText = update.getMessage().getText();
+            sendMessage(chatId, "You said: " + messageText);
+        }
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: {}", e.getMessage());
+        }
+    }
+}
