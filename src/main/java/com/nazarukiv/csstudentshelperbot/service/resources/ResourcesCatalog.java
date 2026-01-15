947dcf1cea73093d3af1c3ab3f030c401373f857
nazarukiv <nazaruk7649@ukr.net>
2026-01-15 12:25:04 +0000

    test: full list of resources added for testing

--- src/main/java/com/nazarukiv/csstudentshelperbot/service/resources/ResourcesCatalog.java
+++ src/main/java/com/nazarukiv/csstudentshelperbot/service/resources/ResourcesCatalog.java
@@ -16,6 +16,33 @@ public class ResourcesCatalog {
                 new ResourceItem("Git Book", "https://git-scm.com/book/en/v2", "Free official book"),
                 new ResourceItem("GitHub Docs", "https://docs.github.com/", "How GitHub works")
         ));
+
+        data.put(ResourceCategory.DATABASES, List.of(
+                new ResourceItem("w3schools SQL tutorial", "https://www.w3schools.com/sql/", "Free online tutorial"),
+                new ResourceItem("SQL roadmap", "https://roadmap.sh/sql", "SQL roadmap")
+        ));
+
+        data.put(ResourceCategory.ALGORITHMS, List.of(
+                new ResourceItem("Data Structures & Algorithms roadmap", "https://roadmap.sh/datastructures-and-algorithms", "algos and ds roadmap")
+        ));
+
+        data.put(ResourceCategory.AI_TOOLS, List.of(
+                new ResourceItem("Chat GPT by OpenAI", "https://chatgpt.com/", "most popular AI tool")
+        ));
+
+
+        data.put(ResourceCategory.JAVA, List.of(
+                new ResourceItem("Java Developer Roadmap", "https://roadmap.sh/java", "java roadmap")
+        ));
+
+        data.put(ResourceCategory.ROADMAPS, List.of(
+                new ResourceItem("All roadmaps", "https://roadmap.sh/", "all roadmaps")
+        ));
+
+
+
+
+
     }
 
     public List<ResourceItem> get(ResourceCategory category) {
